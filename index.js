e9e5d5b70d476b84a4d481d116ef4c8dc5a2c9e0
erik gomez <erik12fgtv@gmail.com>
2022-07-12 16:15:39 -0700

    commit tree c84bb28132d33957607946c1b213b69aad6fcdae

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching (arrayOfWords, string) {
+    return arrayOfWords.filter(word => string.toLowerCase() === word.toLowerCase()) 
+}
+
+function fuzzyMatch (array, letter) {
+    return array.filter(word => word.startsWith(letter))
+}
+
+function matchName (array, string) {
+    return array.filter(object => object.name === string )
+}
